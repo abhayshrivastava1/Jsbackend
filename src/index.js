b359c6c4df034ce92dac52828fcb754bf0e9101e
abhayshrivastava1 <abhay182981@gmail.com>
2025-01-14 15:56:31 +0530

    connection

---
+++ src/index.js
@@ -0,0 +1,56 @@
+// require('dotenv').config({path: './env'})
+
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+// .then(() => {
+//   app.listen(process.env.PORT || 8000, () => {
+//     console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+//   });
+// }).catch((error) => {
+//   console.log("MONGO db connection failed !!! ", error);
+// });
+
+
+
+
+
+
+
+
+// ( async () => {
+//     try {
+//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//       app.on("error", (error) => {
+//         console.log("ERRR: ", error);
+//         throw error;
+//       });
+
+//       app.listen(process.env.PORT, () => {
+//         console.log(`App is listening on port ${process.env.PORT}`);
+//       });
+//     } catch (error) {
+//       console.error("ERROR: ", error);
+//       throw error;
+//     }
+// })();
+
+
+
+
+
+// try catch mein wrap kro 
+// database is always in another continent 
+// always use try catch and async await 
