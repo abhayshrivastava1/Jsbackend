29339b35188b570ac82981dc06444c9a78d84bee
abhayshrivastava1 <abhay182981@gmail.com>
2025-01-16 17:39:50 +0530

    adding user and video models

---
+++ src/models/video.models.js
@@ -0,0 +1,45 @@
+import mongoose, {Schema} from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFIle: {
+      type: String,
+      required: true,
+      thumbnail,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // cloudenaryurl
+      required: true,
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Booleanl,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+    
+  },
+  { timestamps: true }
+);
+
+
+
+export const Video = mongoose.model("Video", videoSchema)
