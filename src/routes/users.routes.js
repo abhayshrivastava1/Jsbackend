31986f142b27f85041d79eae5bfd95c9f4f56c30
abhayshrivastava1 <abhay182981@gmail.com>
2025-03-19 12:09:55 +0530

    change in routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,31 +1,34 @@
 // import { Router } from 'express';
 // import {registerUser} from '../controllers/user.controllers.js'
 
-
 // const router = Router()
 
 // router.route("/register").post(registerUser)
 
-
-
-
 // export default router
-
 
 import express from "express";
 import { upload } from "../middlewares/multer.middleware.js"; // Import multer
 import { registerUser } from "../controllers/user.controllers.js";
 import { loginUser } from "../controllers/user.controllers.js";
 import { logoutUser } from "../controllers/user.controllers.js";
-import { refreshAccessToken } from "../controllers/user.controllers.js";
+import {
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrUser,
+  accountUpdate,
+  updateAvatar,
+  updateCover,
+  getUserChannel,
+  getWatchHistory,
+} from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
 
 const router = express.Router();
 
 // Use multer for file uploads
 // router.route("/register").post(upload.fields([
-//   { name: "avatar", maxCount: 1 }, 
+//   { name: "avatar", maxCount: 1 },
 //   { name: "coverImage", maxCount: 1 }
 // ]), registerUser);
 
@@ -42,7 +45,6 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-
-
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
